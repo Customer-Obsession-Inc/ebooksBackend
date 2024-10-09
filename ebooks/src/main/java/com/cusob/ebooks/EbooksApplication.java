6daaef4ee0297a534b073e4d2200b1592ec7ce20
CDCBLOCK <65992999+CDCBLOCK@users.noreply.github.com>
2024-10-09 10:26:41 +0800

    Add files via upload
    10.09 v1 电子书

---
+++ ebooks/src/main/java/com/cusob/ebooks/EbooksApplication.java
@@ -0,0 +1,19 @@
+package com.cusob.ebooks;
+
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+@EnableTransactionManagement
+@SpringBootApplication
+@MapperScan("com.cusob.ebooks.mapper")
+@EnableAsync
+public class EbooksApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EbooksApplication.class, args);
+    }
+
+}
---
+++ ebooks/src/main/java/com/cusob/ebooks/controller/loginController.java
@@ -0,0 +1,4 @@
+package com.cusob.ebooks.controller;
+
+public class loginController {
+}
